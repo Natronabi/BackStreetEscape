ef046671d5d6dc265f239f026ce3b99ce5490b63
Natronabi <surzaneribol@gmail.com>
2026-02-28 01:52:42 +0300

    v0.01 initial commit: setup player controller

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+
+    Rigidbody rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(moveX, 0f,moveY).normalized* moveSpeed;
+        rb.velocity = movement;
+        
+    }
+}
